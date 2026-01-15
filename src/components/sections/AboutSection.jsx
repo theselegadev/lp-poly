2ef805db92aa900b5fa4617130d6caeb4183cb14
theselegadev <matheus.seleghin7@gmail.com>
2026-01-15 08:57:19 -0300

    ajusta espaçamento about section

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -4,13 +4,13 @@ const AboutSection = () => {
     <section id="about" className="min-h-screen flex items-center py-20 bg-gray-100">
       <div className="container mx-auto px-6">
         <div className="max-w-5xl mx-auto text-center">
-          <h2 className="text-4xl font-extrabold text-gray-900 ">
+          <h2 className="text-4xl font-extrabold text-gray-900 mb-6">
             Feito pela comunidade, para a comunidade
           </h2>
-          <p className="text-xl text-gray-700">
+          <p className="text-xl text-gray-700 mb-7">
             Projeto da comunidade, feito para a comunidade — incentivamos fortemente o aprendizado em programação porque é simplesmente algo muito legal.
           </p>
-          <p className="text-md text-gray-800 ">
+          <p className="text-md text-gray-800 mb-6">
             Projeto iniciado por <span className="text-blue-600">Rodrigo Schio</span>, desenvolvedor principal, com a colaboração de Matheus Seleghin, que desenvolveu a página e Mathias que contribui em c com o projeto.
           </p>
 
